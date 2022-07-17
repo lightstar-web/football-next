9cd806f183b11882d8ea422fe1501f15a8c236cf
lightstar0412 <dev.star.man0412@gmail.com>
2022-07-17 09:56:56 +0100

    do not retry PL API

--- src/hooks/useFixtures.ts
+++ src/hooks/useFixtures.ts
@@ -1,19 +1,14 @@
 import { useQuery } from 'react-query'
 
 const fetchFixtures = async () => {
-  const fixtures = await fetch('https://fantasy.premierleague.com/api/fixtures/', {
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
+  const fixtures = await fetch('https://fantasy.premierleague.com/api/fixtures/')
   const data = await fixtures.json()
-  console.log(data)
   return data
 }
 
 const useFixtures = () => {
   return useQuery(['fixtures'], () => fetchFixtures(), {
-    retry: 1
+    retry: 0
   })
 }
 
