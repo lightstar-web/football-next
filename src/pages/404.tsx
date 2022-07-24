f20f1d05987bc7cbce2a50964aa711f23c03963d
lightstar0412 <dev.star.man0412@gmail.com>
2022-07-24 11:36:17 +0100

    add 500, 404, sentry.io

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Layout from '@/components/Layout/Layout'
+import Head from 'next/head'
+import Heading from '@/components/Heading/Heading'
+import Link from '@/components/Link/Link'
+
+const Custom500 = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Page not found</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
+      <main className="flex flex-col gap-4">
+        <Heading level="1">Sorry - we couldn't find that page</Heading>
+        <Link href="/fixtures">Back to fixtures</Link>
+      </main>
+    </Layout>
+  )
+}
+
+export default Custom500
